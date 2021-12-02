d6ed3a0af01aba710a75fee6ba2e01c3440f959a
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-03 03:02:08 +0600

    feat: add user configuration

---
+++ internal/pkg/config/config.go
@@ -0,0 +1,32 @@
+
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+// UserConfig stores all configuration of user application
+type UserConfig struct {
+	DBDriver string `mapstructure:"DB_DRIVER"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+}
+
+// LoadConfig parses configuration and stores the result in
+// the value pointed to by config.
+func LoadConfig(name string, path string, config interface{}) error {
+	var err error
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName(name)
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	err = viper.Unmarshal(config)
+	return err
+}
