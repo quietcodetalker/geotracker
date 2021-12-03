b61a42769f58174e33efd4f826b626f051505379
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-03 10:49:50 +0600

    feat(location): make repo SetLocation method returns Location entity

--- internal/app/user/core/port/location.go
+++ internal/app/user/core/port/location.go
@@ -1,6 +1,9 @@
 package port
 
-import "context"
+import (
+	"context"
+	"gitlab.com/spacewalker/locations/internal/app/user/core/domain"
+)
 
 // SetLocationArg is a param object of user repository SetLocation method.
 type SetLocationArg struct {
@@ -9,7 +12,9 @@ type SetLocationArg struct {
 	Longitude float64 `json:"longitude"`
 }
 
-type LocationRepo interface {
+// LocationRepository represents location repository.
+type LocationRepository interface {
 	// SetLocation sets user's location by given user ID.
-	SetLocation(ctx context.Context, arg SetLocationArg) error
+	// Returns updated location entity.
+	SetLocation(ctx context.Context, arg SetLocationArg) (domain.Location, error)
 }
