fd11f962571e28f66f84b6074a1e791d7e321ac4
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-05 19:57:33 +0600

    fix: correct typo

--- db/migrations/users/000001_init.down.sql
+++ db/migrations/users/000001_init.down.sql
@@ -1,5 +1,5 @@
-DROP TRIGGER IF EXISTS update_updated_at ON positions;
+DROP TRIGGER IF EXISTS update_updated_at ON locations;
 DROP TRIGGER IF EXISTS update_updated_at ON users;
-DROP TABLE IF EXISTS positions;
+DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS users;
 DROP FUNCTION IF EXISTS update_updated_at;
