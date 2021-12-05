e5f4be53102ffcc5eb1d283c3c9769bdeaa20e83
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-05 19:58:33 +0600

    refactor: name constraints explicitly

--- db/migrations/users/000001_init.up.sql
+++ db/migrations/users/000001_init.up.sql
@@ -7,18 +7,27 @@ END;
 $$ language 'plpgsql';
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username varchar(16) UNIQUE CHECK (LENGTH(username) >= 4 AND username ~ '^[a-zA-Z0-9]+$'),
+    id SERIAL,
+    username varchar(16),
     created_at timestamp DEFAULT current_timestamp NOT NULL,
-    updated_at timestamp DEFAULT current_timestamp NOT NULL
+    updated_at timestamp DEFAULT current_timestamp NOT NULL,
+
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_username_key UNIQUE (username),
+    CONSTRAINT users_username_valid CHECK (LENGTH(username) >= 4 AND username ~ '^[a-zA-Z0-9]+$')
 );
 
-CREATE TABLE positions (
-    user_id INT REFERENCES users (id) PRIMARY KEY,
-    latitude NUMERIC(11, 8) CHECK (latitude >= -180 AND latitude <= 180) NOT NULL,
-    longitude NUMERIC(10, 8) CHECK (longitude >= -90 AND longitude <= 90) NOT NULL,
+CREATE TABLE locations (
+    user_id INT,
+    latitude NUMERIC(11, 8) NOT NULL,
+    longitude NUMERIC(10, 8) NOT NULL,
     created_at timestamp DEFAULT current_timestamp NOT NULL,
-    updated_at timestamp DEFAULT current_timestamp NOT NULL
+    updated_at timestamp DEFAULT current_timestamp NOT NULL,
+
+    CONSTRAINT locations_pkey PRIMARY KEY (user_id),
+    CONSTRAINT locations_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT locations_latitude_valid CHECK (latitude >= -180 AND latitude <= 180),
+    CONSTRAINT locations_longitude_valid CHECK (longitude >= -90 AND longitude <= 90)
 );
 
 CREATE TRIGGER update_updated_at BEFORE UPDATE
@@ -26,5 +35,5 @@ CREATE TRIGGER update_updated_at BEFORE UPDATE
     update_updated_at();
 
 CREATE TRIGGER update_updated_at BEFORE UPDATE
-    ON positions FOR EACH ROW EXECUTE PROCEDURE
+    ON locations FOR EACH ROW EXECUTE PROCEDURE
     update_updated_at();
