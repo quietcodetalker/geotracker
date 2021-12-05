0a7cf8e2b516c4b940d52d17f7f34d1e08565c7f
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-05 19:59:03 +0600

    feat: add errors

--- internal/app/user/core/port/errors.go
+++ internal/app/user/core/port/errors.go
@@ -1,7 +1,28 @@
 package port
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
+	// ErrNotFound error means that requested entity is not found.
 	ErrNotFound = errors.New("not found")
+
+	// ErrAttemptedSettingLocationOfNonExistentUser error means that User with UserID that
+	// SetLocation was called with does not exist.
+	ErrAttemptedSettingLocationOfNonExistentUser = errors.New("attempted setting location of non-existent user")
 )
+
+type InvalidLocationErrorViolation struct {
+	Subject string
+	Value   float64
+}
+
+type InvalidLocationError struct {
+	Violations []InvalidLocationErrorViolation
+}
+
+func (e *InvalidLocationError) Error() string {
+	return fmt.Sprintf("invalid location")
+}
