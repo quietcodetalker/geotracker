33068ac1fbbed44bc166b79583295a68c244fdeb
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-09 01:51:46 +0600

    fix: manage earthdistance and kube psql extensions in migrations

--- db/migrations/users/000001_init.down.sql
+++ db/migrations/users/000001_init.down.sql
@@ -3,3 +3,5 @@ DROP TRIGGER IF EXISTS update_updated_at ON users;
 DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS users;
 DROP FUNCTION IF EXISTS update_updated_at;
+DROP EXTENSION IF EXISTS earthdistance;
+DROP EXTENSION IF EXISTS cube;
