9e3a33bcc96c0323aa72ffcc0c7c10a2fe5635e7
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-15 13:29:02 +0600

    feat: add user service implementation

--- internal/app/user/core/service/user.go
+++ internal/app/user/core/service/user.go
@@ -2,17 +2,40 @@ package service
 
 import (
 	"context"
+	"gitlab.com/spacewalker/locations/internal/app/user/core/domain"
 	"gitlab.com/spacewalker/locations/internal/app/user/core/port"
 )
 
-type userService struct{}
-
-// NewUserService creates instance of UserService and returns it's pointer.
-func NewUserService() port.UserService {
-	return &userService{}
+type userService struct {
+	repo port.UserRepository
 }
 
-// SetUserLocation sets user's location.
+// NewUserService creates instance of UserService and returns it's pointer.
+func NewUserService(repo port.UserRepository) port.UserService {
+	return &userService{
+		repo: repo,
+	}
+}
+
+// SetUserLocation sets user's location by given username.
 func (s *userService) SetUserLocation(ctx context.Context, req port.SetUserLocationRequest) (port.SetUserLocationResponse, error) {
-	panic("implement me")
+	if err := validate.Struct(req); err != nil {
+		// TODO: check different errors?
+		return port.SetUserLocationResponse{}, &port.InvalidArgumentError{}
+	}
+
+	location, err := s.repo.SetUserLocation(ctx, port.SetUserLocationArg{
+		Username: req.Username,
+		Point:    domain.Point{req.Longitude, req.Latitude},
+	})
+	if err != nil {
+		return port.SetUserLocationResponse{}, err
+	}
+
+	return port.SetUserLocationResponse{
+		Latitude:  location.Point.Latitude(),
+		Longitude: location.Point.Longitude(),
+	}, nil
+
+	// TODO: add record to location history via history microservice
 }
