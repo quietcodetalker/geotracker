5810961f6efc87d2d3b6e3506990320e663f40eb
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2021-12-15 13:26:19 +0600

    feat: add lat/long validations

--- internal/pkg/validation/validations.go
+++ internal/pkg/validation/validations.go
@@ -5,9 +5,18 @@ import (
 	"regexp"
 )
 
-var usernameRegexp = regexp.MustCompile("[a-zA-Z0-9]{4,16}")
+var usernameRegexp = regexp.MustCompile("^[a-zA-Z0-9]{4,16}$")
 
 func ValidateUsername(fl validator.FieldLevel) bool {
 	value := fl.Field().String()
 	return usernameRegexp.Match([]byte(value))
+}
+
+func ValidateLongitude(fl validator.FieldLevel) bool {
+	value := fl.Field().Float()
+	return -180.0 <= value && value <= 180.0
+}
+func ValidateLatitude(fl validator.FieldLevel) bool {
+	value := fl.Field().Float()
+	return -90.0 <= value && value <= 90.0
 }
