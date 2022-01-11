08d11bdbb8df7e2c852bb1c76ffc4b6c1466a8d6
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2022-01-11 17:17:08 +0700

    feat: add users cmd file

---
+++ cmd/user/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"gitlab.com/spacewalker/locations/internal/app/user/adapter/in/handler"
+	"gitlab.com/spacewalker/locations/internal/app/user/adapter/out/repository"
+	"gitlab.com/spacewalker/locations/internal/app/user/core/service"
+	"gitlab.com/spacewalker/locations/internal/pkg/config"
+	"gitlab.com/spacewalker/locations/internal/pkg/util"
+	"log"
+	"path"
+	"runtime"
+)
+
+func main() {
+	_, filename, _, _ := runtime.Caller(0)
+	rootDir := path.Join(path.Dir(filename), "../..")
+
+	cfg, err := config.LoadUserConfig(
+		"user",
+		path.Join(rootDir, "configs"),
+	)
+	if err != nil {
+		log.Panicf("failed to load config: %v", err)
+	}
+
+	dbSource := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBSSLMode,
+	)
+	db, err := util.OpenDB(cfg.DBDriver, dbSource)
+	if err != nil {
+		log.Panicf("failed to open db: %v", err)
+	}
+
+	repo := repository.NewPostgresRepository(db)
+	svc := service.NewUserService(repo)
+
+	grpcHandler := handler.NewGRPCHandler(svc)
+	if err := grpcHandler.Start(":50051"); err != nil {
+		log.Panic(err)
+	}
+}
