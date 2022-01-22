4b0e3b0b1152213697a2298d4ebe8c062e784cf8
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2022-01-23 04:41:27 +0700

    test: Cover GetDistance repo method with tests

--- internal/app/history/adapter/out/repository/history_postgres_test.go
+++ internal/app/history/adapter/out/repository/history_postgres_test.go
@@ -8,6 +8,7 @@ import (
 	"gitlab.com/spacewalker/locations/internal/app/history/core/port"
 	"gitlab.com/spacewalker/locations/internal/pkg/geo"
 	"testing"
+	"time"
 )
 
 func (s *PostgresTestSuite) Test_PostgresRepository_AddRecord() {
@@ -219,6 +220,103 @@ func (s *PostgresTestSuite) Test_PostgresRepository_AddRecord() {
 		s.T().Run(tc.name, func(t *testing.T) {
 			rec, err := repo.AddRecord(context.Background(), tc.req)
 			tc.assert(s.T(), rec, err)
+		})
+	}
+}
+
+func (s *PostgresTestSuite) Test_PostgresRepository_GetDistance() {
+	ref := time.Now()
+	records := []domain.Record{
+		{
+			UserID:    1,
+			A:         geo.Point{0.0, 0.0},
+			B:         geo.Point{1.0, 0.0},
+			CreatedAt: ref.Add(-time.Hour * 5),
+		},
+		{
+			UserID:    1,
+			A:         geo.Point{1.0, 0.0},
+			B:         geo.Point{1.0, 1.0},
+			CreatedAt: ref.Add(-time.Hour * 4),
+		},
+		{
+			UserID:    2,
+			A:         geo.Point{1.0, 1.0},
+			B:         geo.Point{2.0, 1.0},
+			CreatedAt: ref.Add(-time.Hour * 3),
+		},
+		{
+			UserID:    1,
+			A:         geo.Point{2.0, 1.0},
+			B:         geo.Point{2.0, 2.0},
+			CreatedAt: ref.Add(-time.Hour * 2),
+		},
+	}
+
+	s.seedRecords(records)
+
+	testCases := []struct {
+		name   string
+		req    port.HistoryRepositoryGetDistanceRequest
+		assert func(t *testing.T, distance float64, err error)
+	}{
+		{
+			name: "OK_NoRecords",
+			req: port.HistoryRepositoryGetDistanceRequest{
+				UserID: 3,
+				From:   ref.Add(-10 * time.Hour),
+				To:     ref.Add(10 * time.Hour),
+			},
+			assert: func(t *testing.T, distance float64, err error) {
+				require.NoError(t, err)
+				require.Equal(t, 0.0, distance)
+			},
+		},
+		{
+			name: "OK",
+			req: port.HistoryRepositoryGetDistanceRequest{
+				UserID: 1,
+				From:   ref.Add(-10 * time.Hour),
+				To:     ref.Add(10 * time.Hour),
+			},
+			assert: func(t *testing.T, distance float64, err error) {
+				require.NoError(t, err)
+				require.InDelta(t, 111194.6977316823*3, distance, 0.00000001)
+			},
+		},
+		{
+			name: "OK_TwoRecordsInTimeFrame",
+			req: port.HistoryRepositoryGetDistanceRequest{
+				UserID: 1,
+				From:   ref.Add(-10 * time.Hour),
+				To:     ref.Add(-3 * time.Hour),
+			},
+			assert: func(t *testing.T, distance float64, err error) {
+				require.NoError(t, err)
+				require.InDelta(t, 111194.6977316823*2, distance, 0.00000001)
+			},
+		},
+		{
+			name: "OK_NoRecordsInTimeFrame",
+			req: port.HistoryRepositoryGetDistanceRequest{
+				UserID: 1,
+				From:   ref.Add(-10 * time.Hour),
+				To:     ref.Add(-6 * time.Hour),
+			},
+			assert: func(t *testing.T, distance float64, err error) {
+				require.NoError(t, err)
+				require.Equal(t, 0.0, distance)
+			},
+		},
+	}
+
+	repo := repository.NewPostgresRepository(s.db)
+
+	for _, tc := range testCases {
+		tc := tc
+		s.T().Run(tc.name, func(t *testing.T) {
+			distance, err := repo.GetDistance(context.Background(), tc.req)
+			tc.assert(s.T(), distance, err)
 		})
 	}
 }
