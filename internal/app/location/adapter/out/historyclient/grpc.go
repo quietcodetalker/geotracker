725a84c4bab02b181e11a4db1d7bad42efbe95fa
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2022-01-25 02:11:42 +0700

    feat: Add history grpc client

---
+++ internal/app/location/adapter/out/historyclient/grpc.go
@@ -0,0 +1,56 @@
+package historyclient
+
+import (
+	"context"
+	"gitlab.com/spacewalker/locations/internal/app/location/core/port"
+	"gitlab.com/spacewalker/locations/internal/pkg/geo"
+	pb "gitlab.com/spacewalker/locations/pkg/api/proto/v1/history"
+	"google.golang.org/grpc"
+)
+
+type GRPCClient struct{}
+
+func NewGRPCClient() port.HistoryClient {
+	return &GRPCClient{}
+}
+
+func (G GRPCClient) AddRecord(ctx context.Context, req port.HistoryClientAddRecordRequest) (port.HistoryClientAddRecordResponse, error) {
+	var opts []grpc.DialOption
+
+	opts = append(opts, grpc.WithInsecure())
+
+	conn, err := grpc.Dial("localhost:50052", opts...)
+	if err != nil {
+		return port.HistoryClientAddRecordResponse{}, err
+	}
+	defer conn.Close()
+
+	client := pb.NewHistoryClient(conn)
+
+	res, err := client.AddRecord(ctx, &pb.AddRecordRequest{
+		UserId: int32(req.UserID),
+		A: &pb.Point{
+			Longitude: req.A.Longitude(),
+			Latitude:  req.A.Latitude(),
+		},
+		B: &pb.Point{
+			Longitude: req.B.Longitude(),
+			Latitude:  req.B.Latitude(),
+		},
+	})
+	if err != nil {
+		return port.HistoryClientAddRecordResponse{}, err
+	}
+
+	return port.HistoryClientAddRecordResponse{
+		UserID:    int(res.UserId),
+		A:         geo.Point{res.A.Longitude, res.A.Latitude},
+		B:         geo.Point{res.B.Longitude, res.B.Latitude},
+		CreatedAt: res.CreatedAt.AsTime(),
+		UpdatedAt: res.UpdatedAt.AsTime(),
+	}, nil
+}
+
+func (G GRPCClient) GetDistance(ctx context.Context, req port.HistoryClientGetDistanceRequest) (port.HistoryClientGetDistanceResponse, error) {
+	panic("implement me")
+}
