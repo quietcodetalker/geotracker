11264ac9ee359ce77d7522e202201eca551018ca
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2022-02-02 23:04:37 +0700

    feat: Update LoggerUnaryServerInterceptor
    Handle tracingID in the interceptor
    Log tracingID

--- internal/pkg/middleware/grpc.go
+++ internal/pkg/middleware/grpc.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 	"gitlab.com/spacewalker/locations/internal/pkg/log"
+	"gitlab.com/spacewalker/locations/internal/pkg/util"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 	"time"
 )
 
+// LoggerUnaryServerInterceptor TODO: description
 func LoggerUnaryServerInterceptor(logger log.Logger) func(
 	ctx context.Context,
 	req interface{},
@@ -25,15 +27,22 @@ func LoggerUnaryServerInterceptor(logger log.Logger) func(
 		fullMethod := info.FullMethod
 		request := fmt.Sprintf("%v", req)
 
+		traceID, ok := util.GetTraceIDFromMetadata(ctx)
+		if !ok {
+			traceID = util.GenerateTraceID()
+		}
+		util.AddTraceIDToCtx(ctx, traceID)
+
 		m, err := handler(ctx, req)
 
 		st, _ := status.FromError(err)
 
-		logger.Info("grpc request complete", log.Fields{
+		logger.Info("incoming grpc request complete", log.Fields{
 			"method":   fullMethod,
 			"duration": time.Since(start),
 			"code":     st.Code(),
 			"request":  request,
+			"trace-id": traceID,
 		})
 
 		return m, err
