e99b6054ba081cbe7c67bb071526dd4fa0ca02e3
Vladimir Tkachenko <unknown.spacewalker@gmail.com>
2022-04-11 23:05:02 +0600

    Add code generation directive

--- internal/app/history/core/port/location.go
+++ internal/app/history/core/port/location.go
@@ -1,8 +1,10 @@
+//go:generate mockgen -destination=mock/mock_location.go -package=mock . LocationClient
+
 package port
 
 import "context"
 
 // LocationClient TODO: add description
 type LocationClient interface {
-  GetUserIDByUsername(ctx context.Context, username string) (int, error)
+	GetUserIDByUsername(ctx context.Context, username string) (int, error)
 }
